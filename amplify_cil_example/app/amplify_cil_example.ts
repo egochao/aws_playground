2ab2429b4f7045fbcdf32de142028b451c5a1607
egochao <johnoldmanworkspace@gmail.com>
2022-09-01 05:53:58 +0000

    Update folder name

--- amplify_cil_example/app/amplify_cil_example.ts
+++ amplify_cil_example/app/amplify_cil_example.ts
@@ -2,6 +2,9 @@
 import * as cdk from 'aws-cdk-lib';
 
 import { CognitoStack } from '../stack/cognito_stack';
+import { StaticWebStack } from '../stack/static_web_stack';
+
 
 const app = new cdk.App();
 new CognitoStack(app, 'CognitoStack');
+new StaticWebStack(app, 'StaticWebStack');
