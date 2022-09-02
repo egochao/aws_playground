eb1b06896d1453c2e83f8ee7a4e539e5b3f5be56
egochao <johnoldmanworkspace@gmail.com>
2022-09-02 07:28:10 +0000

    remove secret stack

--- amplify_cil_example/bin/simple_static_website_components.ts
+++ amplify_cil_example/bin/simple_static_website_components.ts
@@ -3,10 +3,8 @@ import * as cdk from 'aws-cdk-lib';
 
 import { CognitoStack } from '../lib/cognito_stack';
 import { StaticWebStack } from '../lib/static_web_stack';
-import { SecretsStack } from '../lib/secret_stack';
 
 const app = new cdk.App();
-const secrets = new SecretsStack(app, 'SecretsStack');
 
 const authStack = new CognitoStack(app, 'CognitoStack');
 
--- amplify_cil_example/lib/static_web_stack.ts
+++ amplify_cil_example/lib/static_web_stack.ts
@@ -7,25 +7,28 @@ export class StaticWebStack extends cdk.Stack {
     constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
         const githubSecret = new secretsmanager.Secret(this, 'githubSecret')
-          
-
+        const githubOwner = 'egochao';
+        const githubRepo = 'simple_gatsby_blog';
+        const githubProdBranch = 'master';
+        const githubDevBranch = 'devevelop';
+        
         const githubToken = secretsmanager.Secret.fromSecretNameV2(
             this, 'githubToken', githubSecret.secretName).secretValue;
         const amplifyApp = new amplify.App(this, 'StaticWebApp', {
             sourceCodeProvider: new amplify.GitHubSourceCodeProvider({
-                owner: "egochao",
-                repository: "simple_gatsby_blog",
+                owner: githubOwner,
+                repository: githubRepo,
                 oauthToken: githubToken,
             }),
         });
-        const masterBranch = amplifyApp.addBranch("master");
-        const developBranch = amplifyApp.addBranch("develop");
+        const prodBranch = amplifyApp.addBranch(githubProdBranch);
+        const developBranch = amplifyApp.addBranch(githubDevBranch);
 
-        new cdk.CfnOutput(this, 'secretName', {
-            value: githubSecret.secretName,
-        });
-        new cdk.CfnOutput(this, 'secretArn', {
+        new cdk.CfnOutput(this, 'githubTokenSecretArn', {
             value: githubSecret.secretArn,
+        });
+        new cdk.CfnOutput(this, 'SiteUrl', {
+            value: amplifyApp.defaultDomain,
         });
         
     }
