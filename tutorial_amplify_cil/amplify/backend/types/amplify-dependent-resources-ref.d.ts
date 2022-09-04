3bacf48d7452e083172c1153d39eb0ca570d2305
egochao <johnoldmanworkspace@gmail.com>
2022-09-04 11:31:40 +0000

    remove old code

--- tutorial_amplify_cil/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "tutorialamplifycil": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
