b3d93fad1c2dcfa62c21dbd8335518c57a453c6e
egochao <johnoldmanworkspace@gmail.com>
2022-09-04 11:55:57 +0000

    add auth

--- tutorial_amplify_cil/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ tutorial_amplify_cil/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,1 +1,11 @@
-export type AmplifyDependentResourcesAttributes = {}
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "tutorialamplifyv2433f8bfd433f8bfd": {
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
